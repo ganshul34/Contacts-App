82f7e101db63e75474e0af8d18cd0eb71b9e8de7
Anshul <ganshul34@gmail.com>
2019-04-03 10:12:08 +0530

    Build the static version of the App

---
+++ docs/build/precache-manifest.33b43312a8da018a2351014c7aed5db7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f4f266a25d3a1d37993",
+    "url": "/Contacts-App/static/js/runtime~main.69a7773d.js"
+  },
+  {
+    "revision": "2b19352f3b207b9ec989",
+    "url": "/Contacts-App/static/js/main.f1aae5ef.chunk.js"
+  },
+  {
+    "revision": "6665331487e28d763d3f",
+    "url": "/Contacts-App/static/js/2.5ca56e5e.chunk.js"
+  },
+  {
+    "revision": "2b19352f3b207b9ec989",
+    "url": "/Contacts-App/static/css/main.60c1bbf4.chunk.css"
+  },
+  {
+    "revision": "bf1abbc1819826ed5e5b9442a1bd827a",
+    "url": "/Contacts-App/index.html"
+  }
+];
